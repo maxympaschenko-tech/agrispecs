89a0e97b9147149cfd2d84f62ea307a13e1ae2e3
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-28 13:32:38 +0300

    Register Case IH Steiger migration

--- lib/migrations/case-ih.ts
+++ lib/migrations/case-ih.ts
@@ -3,10 +3,12 @@ import { caseIHAfsConnectPumaCurrentSpecsMigration } from '@/lib/migrations/2026
 import { caseIHAfsConnectPumaL117LoaderMigration } from '@/lib/migrations/20260828_228_case_ih_afs_connect_puma_l117_loader';
 import { caseIHMagnumCurrentSpecsMigration } from '@/lib/migrations/20260828_229_case_ih_magnum_current_specs';
 import { caseIHMagnumL118LoaderMigration } from '@/lib/migrations/20260828_230_case_ih_magnum_l118_loader';
+import { caseIHSteigerCurrentSpecsMigration } from '@/lib/migrations/20260828_231_case_ih_steiger_current_specs';
 
 export const caseIHMigrations: DbMigration[] = [
   caseIHAfsConnectPumaCurrentSpecsMigration,
   caseIHAfsConnectPumaL117LoaderMigration,
   caseIHMagnumCurrentSpecsMigration,
   caseIHMagnumL118LoaderMigration,
+  caseIHSteigerCurrentSpecsMigration,
 ];
