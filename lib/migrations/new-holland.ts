7970b2035e32cbd9e8ccfe92018e9fc5a8221962
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-28 22:49:50 +0300

    Register New Holland Workmaster migration

--- lib/migrations/new-holland.ts
+++ lib/migrations/new-holland.ts
@@ -1,6 +1,8 @@
 import type { DbMigration } from '@/lib/db-migration-types';
 import { newHollandPowerStarCurrentSpecsMigration } from '@/lib/migrations/20260828_272_new_holland_powerstar_current_specs';
+import { newHollandWorkmaster5070CurrentSpecsMigration } from '@/lib/migrations/20260828_273_new_holland_workmaster_50_70_current_specs';
 
 export const newHollandMigrations: DbMigration[] = [
   newHollandPowerStarCurrentSpecsMigration,
+  newHollandWorkmaster5070CurrentSpecsMigration,
 ];
