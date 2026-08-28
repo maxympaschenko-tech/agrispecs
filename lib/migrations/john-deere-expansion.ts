df190f081ff33e6270079ae6e93b966ac5d48066
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-28 12:21:17 +0300

    Register John Deere 8R expansion migrations

--- lib/migrations/john-deere-expansion.ts
+++ lib/migrations/john-deere-expansion.ts
@@ -1,8 +1,12 @@
 import type { DbMigration } from '@/lib/db-migration-types';
 import { johnDeere7RCurrentSpecsMigration } from '@/lib/migrations/20260828_211_john_deere_7r_current_specs';
 import { johnDeere7RServiceFiltersMigration } from '@/lib/migrations/20260828_212_john_deere_7r_service_filters';
+import { johnDeere8RCurrentSpecsMigration } from '@/lib/migrations/20260828_213_john_deere_8r_current_specs';
+import { johnDeere8RServiceFiltersMigration } from '@/lib/migrations/20260828_214_john_deere_8r_service_filters';
 
 export const johnDeereExpansionMigrations: DbMigration[] = [
   johnDeere7RCurrentSpecsMigration,
   johnDeere7RServiceFiltersMigration,
+  johnDeere8RCurrentSpecsMigration,
+  johnDeere8RServiceFiltersMigration,
 ];
