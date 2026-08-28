4b3fb31b23bfe486eb28803d378af20691896fe3
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-28 12:39:29 +0300

    Register John Deere 8RT and 8RX migrations

--- lib/migrations/john-deere-expansion.ts
+++ lib/migrations/john-deere-expansion.ts
@@ -3,10 +3,18 @@ import { johnDeere7RCurrentSpecsMigration } from '@/lib/migrations/20260828_211_
 import { johnDeere7RServiceFiltersMigration } from '@/lib/migrations/20260828_212_john_deere_7r_service_filters';
 import { johnDeere8RCurrentSpecsMigration } from '@/lib/migrations/20260828_213_john_deere_8r_current_specs';
 import { johnDeere8RServiceFiltersMigration } from '@/lib/migrations/20260828_214_john_deere_8r_service_filters';
+import { johnDeere8RTCurrentSpecsMigration } from '@/lib/migrations/20260828_215_john_deere_8rt_current_specs';
+import { johnDeere8RTServiceFiltersMigration } from '@/lib/migrations/20260828_216_john_deere_8rt_service_filters';
+import { johnDeere8RXCurrentSpecsMigration } from '@/lib/migrations/20260828_217_john_deere_8rx_current_specs';
+import { johnDeere8RXServiceFiltersMigration } from '@/lib/migrations/20260828_218_john_deere_8rx_service_filters';
 
 export const johnDeereExpansionMigrations: DbMigration[] = [
   johnDeere7RCurrentSpecsMigration,
   johnDeere7RServiceFiltersMigration,
   johnDeere8RCurrentSpecsMigration,
   johnDeere8RServiceFiltersMigration,
+  johnDeere8RTCurrentSpecsMigration,
+  johnDeere8RTServiceFiltersMigration,
+  johnDeere8RXCurrentSpecsMigration,
+  johnDeere8RXServiceFiltersMigration,
 ];
