07d08da937e963745a059408ef918aa4b49d5487
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-29 22:01:13 +0300

    Add Massey Ferguson loader fitment
    Add verified MF 911X, MF 931X and MF 936X utility loader specs and official current US machine fitment.

--- lib/migrations/massey-ferguson.ts
+++ lib/migrations/massey-ferguson.ts
@@ -1,8 +1,10 @@
 import type { DbMigration } from '@/lib/db-migration-types';
 import { masseyFergusonCurrentUsCoreMigration } from '@/lib/migrations/20260829_300_massey_ferguson_current_us_core';
 import { masseyFergusonUsExpansionMigration } from '@/lib/migrations/20260829_301_massey_ferguson_us_expansion';
+import { masseyFergusonUtilityLoadersMigration } from '@/lib/migrations/20260829_302_massey_ferguson_utility_loaders';
 
 export const masseyFergusonMigrations: DbMigration[] = [
   masseyFergusonCurrentUsCoreMigration,
   masseyFergusonUsExpansionMigration,
+  masseyFergusonUtilityLoadersMigration,
 ];
