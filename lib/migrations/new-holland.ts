b0afe80557b8735f477837d3618a1d10c644e213
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-29 09:29:57 +0300

    Register New Holland T6 migration

--- lib/migrations/new-holland.ts
+++ lib/migrations/new-holland.ts
@@ -8,6 +8,7 @@ import { newHollandWorkmaster35C40CCurrentSpecsMigration } from '@/lib/migration
 import { newHollandT4FSCurrentSpecsMigration } from '@/lib/migrations/20260829_278_new_holland_t4fs_current_specs';
 import { newHollandT4FCurrentSpecsMigration } from '@/lib/migrations/20260829_279_new_holland_t4f_current_specs';
 import { newHollandT5CurrentSpecsMigration } from '@/lib/migrations/20260829_280_new_holland_t5_current_specs';
+import { newHollandT6CurrentSpecsMigration } from '@/lib/migrations/20260829_281_new_holland_t6_current_specs';
 
 export const newHollandMigrations: DbMigration[] = [
   newHollandPowerStarCurrentSpecsMigration,
@@ -19,4 +20,5 @@ export const newHollandMigrations: DbMigration[] = [
   newHollandT4FSCurrentSpecsMigration,
   newHollandT4FCurrentSpecsMigration,
   newHollandT5CurrentSpecsMigration,
+  newHollandT6CurrentSpecsMigration,
 ];
