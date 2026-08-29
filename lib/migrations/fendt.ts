8fb54ec7d62fded4c98febe473162f05c91e98c3
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-29 22:48:05 +0300

    Register Fendt 600 Vario migration

--- lib/migrations/fendt.ts
+++ lib/migrations/fendt.ts
@@ -1,6 +1,8 @@
 import type { DbMigration } from '@/lib/db-migration-types';
 import { fendtCurrentUsCoreMigration } from '@/lib/migrations/20260829_310_fendt_current_us_core';
+import { fendt600VarioCurrentUsMigration } from '@/lib/migrations/20260829_311_fendt_600_vario_current_us';
 
 export const fendtMigrations: DbMigration[] = [
   fendtCurrentUsCoreMigration,
+  fendt600VarioCurrentUsMigration,
 ];
