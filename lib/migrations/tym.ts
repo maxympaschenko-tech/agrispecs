98f4107d33c9472a16ee17a0d2b49628aaeb697d
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-30 16:58:30 +0300

    Register TYM Series 2 migration

--- lib/migrations/tym.ts
+++ lib/migrations/tym.ts
@@ -1,6 +1,8 @@
 import type { DbMigration } from '@/lib/db-migration-types';
 import { tymSeries1CurrentUsMigration } from '@/lib/migrations/20260830_398_tym_series1_current_us';
+import { tymSeries2CurrentUsMigration } from '@/lib/migrations/20260830_399_tym_series2_current_us';
 
 export const tymMigrations: DbMigration[] = [
   tymSeries1CurrentUsMigration,
+  tymSeries2CurrentUsMigration,
 ];
