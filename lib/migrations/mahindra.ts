0f11512e5f8c2b0af60dc26fdd15f89e955993cc
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-30 10:10:40 +0300

    Register Mahindra maintenance migration

--- lib/migrations/mahindra.ts
+++ lib/migrations/mahindra.ts
@@ -2,9 +2,11 @@ import type { DbMigration } from '@/lib/db-migration-types';
 import { mahindra11002100CurrentUsMigration } from '@/lib/migrations/20260830_320_mahindra_1100_2100_current_us';
 import { mahindra1600CurrentUsMigration } from '@/lib/migrations/20260830_321_mahindra_1600_current_us';
 import { mahindraUtilityCurrentUsMigration } from '@/lib/migrations/20260830_322_mahindra_utility_current_us';
+import { mahindraMaintenanceIntervalsMigration } from '@/lib/migrations/20260830_323_mahindra_maintenance_intervals';
 
 export const mahindraMigrations: DbMigration[] = [
   mahindra11002100CurrentUsMigration,
   mahindra1600CurrentUsMigration,
   mahindraUtilityCurrentUsMigration,
+  mahindraMaintenanceIntervalsMigration,
 ];
