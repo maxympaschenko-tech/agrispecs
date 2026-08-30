5d8634fdff0d1d406917e82bd13778669fd72d84
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-30 19:02:50 +0300

    Register Versatile MFWD migration

--- lib/migrations/versatile.ts
+++ lib/migrations/versatile.ts
@@ -1,6 +1,8 @@
 import type { DbMigration } from '@/lib/db-migration-types';
 import { versatileNemesisCurrentUsMigration } from '@/lib/migrations/20260830_432_versatile_nemesis_current_us';
+import { versatileMfwdCurrentUsMigration } from '@/lib/migrations/20260830_433_versatile_mfwd_current_us';
 
 export const versatileMigrations: DbMigration[] = [
   versatileNemesisCurrentUsMigration,
+  versatileMfwdCurrentUsMigration,
 ];
