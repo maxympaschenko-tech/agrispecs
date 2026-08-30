777d53a5df33c334831f09e92a9726325cdd6ac7
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-30 17:39:48 +0300

    Register Deutz-Fahr 5G TB migration

--- lib/migrations/deutz-fahr.ts
+++ lib/migrations/deutz-fahr.ts
@@ -3,10 +3,12 @@ import { deutzFahr4SeriesCurrentUsMigration } from '@/lib/migrations/20260830_41
 import { deutzFahr5dKeylineCurrentUsMigration } from '@/lib/migrations/20260830_414_deutz_fahr_5d_keyline_current_us';
 import { deutzFahr5SeriesCurrentUsMigration } from '@/lib/migrations/20260830_415_deutz_fahr_5_series_current_us';
 import { deutzFahr5SeriesLoaderMigration } from '@/lib/migrations/20260830_416_deutz_fahr_5_series_loader';
+import { deutzFahr5gTbCurrentUsMigration } from '@/lib/migrations/20260830_417_deutz_fahr_5g_tb_current_us';
 
 export const deutzFahrMigrations: DbMigration[] = [
   deutzFahr4SeriesCurrentUsMigration,
   deutzFahr5dKeylineCurrentUsMigration,
   deutzFahr5SeriesCurrentUsMigration,
   deutzFahr5SeriesLoaderMigration,
+  deutzFahr5gTbCurrentUsMigration,
 ];
