e21bdb851ef9f7200d550a30bd0cce1b6ca8ef03
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-30 15:07:07 +0300

    Register LS Tractor MT1 loaders

--- lib/migrations/ls-tractor.ts
+++ lib/migrations/ls-tractor.ts
@@ -1,6 +1,8 @@
 import type { DbMigration } from '@/lib/db-migration-types';
 import { lsTractorMt1CurrentUsMigration } from '@/lib/migrations/20260830_370_ls_tractor_mt1_current_us';
+import { lsTractorMt1LoadersMigration } from '@/lib/migrations/20260830_371_ls_tractor_mt1_loaders';
 
 export const lsTractorMigrations: DbMigration[] = [
   lsTractorMt1CurrentUsMigration,
+  lsTractorMt1LoadersMigration,
 ];
