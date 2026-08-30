27f003e323af574c9459e016b3105fcd403724e1
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-30 18:57:09 +0300

    Register CLAAS AXION 900 migration

--- lib/migrations/claas.ts
+++ lib/migrations/claas.ts
@@ -1,8 +1,10 @@
 import type { DbMigration } from '@/lib/db-migration-types';
 import { claasArion600CurrentUsMigration } from '@/lib/migrations/20260830_426_claas_arion_600_current_us';
 import { claasAxion800CurrentUsMigration } from '@/lib/migrations/20260830_427_claas_axion_800_current_us';
+import { claasAxion900CurrentUsMigration } from '@/lib/migrations/20260830_428_claas_axion_900_current_us';
 
 export const claasMigrations: DbMigration[] = [
   claasArion600CurrentUsMigration,
   claasAxion800CurrentUsMigration,
+  claasAxion900CurrentUsMigration,
 ];
