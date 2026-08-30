e886300e954e6f1380ddfea8a1d6be216237507c
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-30 14:03:03 +0300

    Register Bobcat 1000 Platform migration

--- lib/migrations/bobcat.ts
+++ lib/migrations/bobcat.ts
@@ -2,9 +2,11 @@ import type { DbMigration } from '@/lib/db-migration-types';
 import { bobcat2000CurrentUsMigration } from '@/lib/migrations/20260830_360_bobcat_2000_current_us';
 import { bobcat4000CurrentUsMigration } from '@/lib/migrations/20260830_361_bobcat_4000_current_us';
 import { bobcat5000CurrentUsMigration } from '@/lib/migrations/20260830_362_bobcat_5000_current_us';
+import { bobcat1000CurrentUsMigration } from '@/lib/migrations/20260830_363_bobcat_1000_current_us';
 
 export const bobcatMigrations: DbMigration[] = [
   bobcat2000CurrentUsMigration,
   bobcat4000CurrentUsMigration,
   bobcat5000CurrentUsMigration,
+  bobcat1000CurrentUsMigration,
 ];
