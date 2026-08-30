f61b85842eb7ea784047de11dba61beb1e5d4187
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-30 10:26:36 +0300

    Register KIOTI CX2510 migration

--- lib/migrations/kioti.ts
+++ lib/migrations/kioti.ts
@@ -3,10 +3,12 @@ import { kiotiCk20CurrentUsMigration } from '@/lib/migrations/20260830_330_kioti
 import { kiotiCk20LoadersMigration } from '@/lib/migrations/20260830_331_kioti_ck20_loaders';
 import { kiotiCsCurrentUsMigration } from '@/lib/migrations/20260830_332_kioti_cs_current_us';
 import { kiotiCsLoadersMigration } from '@/lib/migrations/20260830_333_kioti_cs_loaders';
+import { kiotiCx2510CurrentUsMigration } from '@/lib/migrations/20260830_334_kioti_cx2510_current_us';
 
 export const kiotiMigrations: DbMigration[] = [
   kiotiCk20CurrentUsMigration,
   kiotiCk20LoadersMigration,
   kiotiCsCurrentUsMigration,
   kiotiCsLoadersMigration,
+  kiotiCx2510CurrentUsMigration,
 ];
