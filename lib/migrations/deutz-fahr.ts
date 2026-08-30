f5a96936fb3c136ac0b463ff106fdf886a6cf23b
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-30 17:36:19 +0300

    Register Deutz-Fahr 5D Keyline migration

--- lib/migrations/deutz-fahr.ts
+++ lib/migrations/deutz-fahr.ts
@@ -1,6 +1,8 @@
 import type { DbMigration } from '@/lib/db-migration-types';
 import { deutzFahr4SeriesCurrentUsMigration } from '@/lib/migrations/20260830_413_deutz_fahr_4_series_current_us';
+import { deutzFahr5dKeylineCurrentUsMigration } from '@/lib/migrations/20260830_414_deutz_fahr_5d_keyline_current_us';
 
 export const deutzFahrMigrations: DbMigration[] = [
   deutzFahr4SeriesCurrentUsMigration,
+  deutzFahr5dKeylineCurrentUsMigration,
 ];
