e1c28a49f119bb2453a29b66c7d04d3297aef7d0
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-30 09:27:37 +0300

    Add current US Fendt 800 Vario Gen5
    * Add current US Fendt 800 Vario Gen5
    
    * Register Fendt 800 Vario Gen5 migration

--- lib/migrations/fendt.ts
+++ lib/migrations/fendt.ts
@@ -3,10 +3,12 @@ import { fendtCurrentUsCoreMigration } from '@/lib/migrations/20260829_310_fendt
 import { fendt600VarioCurrentUsMigration } from '@/lib/migrations/20260829_311_fendt_600_vario_current_us';
 import { fendt1000VarioGen4CurrentUsMigration } from '@/lib/migrations/20260829_312_fendt_1000_vario_gen4_current_us';
 import { fendt300VarioGen4CurrentUsMigration } from '@/lib/migrations/20260829_313_fendt_300_vario_gen4_current_us';
+import { fendt800VarioGen5CurrentUsMigration } from '@/lib/migrations/20260829_314_fendt_800_vario_gen5_current_us';
 
 export const fendtMigrations: DbMigration[] = [
   fendtCurrentUsCoreMigration,
   fendt600VarioCurrentUsMigration,
   fendt1000VarioGen4CurrentUsMigration,
   fendt300VarioGen4CurrentUsMigration,
+  fendt800VarioGen5CurrentUsMigration,
 ];
