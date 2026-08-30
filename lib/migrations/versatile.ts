441c1281254a4ff26801f0e1d54b8558aab28322
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-30 19:07:16 +0300

    Register Versatile DeltaTrack migration

--- lib/migrations/versatile.ts
+++ lib/migrations/versatile.ts
@@ -2,9 +2,11 @@ import type { DbMigration } from '@/lib/db-migration-types';
 import { versatileNemesisCurrentUsMigration } from '@/lib/migrations/20260830_432_versatile_nemesis_current_us';
 import { versatileMfwdCurrentUsMigration } from '@/lib/migrations/20260830_433_versatile_mfwd_current_us';
 import { versatile4wdCurrentUsMigration } from '@/lib/migrations/20260830_434_versatile_4wd_current_us';
+import { versatileDeltaTrackCurrentUsMigration } from '@/lib/migrations/20260830_435_versatile_deltatrack_current_us';
 
 export const versatileMigrations: DbMigration[] = [
   versatileNemesisCurrentUsMigration,
   versatileMfwdCurrentUsMigration,
   versatile4wdCurrentUsMigration,
+  versatileDeltaTrackCurrentUsMigration,
 ];
