c3f8abd061859040f6056d100eb7ee844c2bd160
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-30 13:02:37 +0300

    Register Yanmar YT2 and YM3 migration

--- lib/migrations/yanmar.ts
+++ lib/migrations/yanmar.ts
@@ -1,8 +1,10 @@
 import type { DbMigration } from '@/lib/db-migration-types';
 import { yanmarSaYm2CurrentUsMigration } from '@/lib/migrations/20260830_350_yanmar_sa_ym2_current_us';
 import { yanmarSaYm2LoadersMigration } from '@/lib/migrations/20260830_351_yanmar_sa_ym2_loaders';
+import { yanmarYt2Ym3CurrentUsMigration } from '@/lib/migrations/20260830_352_yanmar_yt2_ym3_current_us';
 
 export const yanmarMigrations: DbMigration[] = [
   yanmarSaYm2CurrentUsMigration,
   yanmarSaYm2LoadersMigration,
+  yanmarYt2Ym3CurrentUsMigration,
 ];
