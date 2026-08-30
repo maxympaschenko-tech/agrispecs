eab2fc8e114a451d15eca77fa2c1149df039cf16
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-30 10:22:17 +0300

    Add current US KIOTI CS Series (#24)
    * Add current US KIOTI CS tractors
    
    * Register KIOTI CS migration

--- lib/migrations/kioti.ts
+++ lib/migrations/kioti.ts
@@ -1,8 +1,10 @@
 import type { DbMigration } from '@/lib/db-migration-types';
 import { kiotiCk20CurrentUsMigration } from '@/lib/migrations/20260830_330_kioti_ck20_current_us';
 import { kiotiCk20LoadersMigration } from '@/lib/migrations/20260830_331_kioti_ck20_loaders';
+import { kiotiCsCurrentUsMigration } from '@/lib/migrations/20260830_332_kioti_cs_current_us';
 
 export const kiotiMigrations: DbMigration[] = [
   kiotiCk20CurrentUsMigration,
   kiotiCk20LoadersMigration,
+  kiotiCsCurrentUsMigration,
 ];
