dbd1058977f01649a9466b56568fee257100b340
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-30 13:59:17 +0300

    Add current US Bobcat 4000 Platform tractors
    * Add current US Bobcat 4000 Platform tractors
    
    * Register Bobcat 4000 Platform migration

--- lib/migrations/bobcat.ts
+++ lib/migrations/bobcat.ts
@@ -1,6 +1,8 @@
 import type { DbMigration } from '@/lib/db-migration-types';
 import { bobcat2000CurrentUsMigration } from '@/lib/migrations/20260830_360_bobcat_2000_current_us';
+import { bobcat4000CurrentUsMigration } from '@/lib/migrations/20260830_361_bobcat_4000_current_us';
 
 export const bobcatMigrations: DbMigration[] = [
   bobcat2000CurrentUsMigration,
+  bobcat4000CurrentUsMigration,
 ];
