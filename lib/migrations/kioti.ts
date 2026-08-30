6093c374908427513bdef1b00d8c0d3bf0ee8a23
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-30 10:24:07 +0300

    Register KIOTI CS loaders

--- lib/migrations/kioti.ts
+++ lib/migrations/kioti.ts
@@ -2,9 +2,11 @@ import type { DbMigration } from '@/lib/db-migration-types';
 import { kiotiCk20CurrentUsMigration } from '@/lib/migrations/20260830_330_kioti_ck20_current_us';
 import { kiotiCk20LoadersMigration } from '@/lib/migrations/20260830_331_kioti_ck20_loaders';
 import { kiotiCsCurrentUsMigration } from '@/lib/migrations/20260830_332_kioti_cs_current_us';
+import { kiotiCsLoadersMigration } from '@/lib/migrations/20260830_333_kioti_cs_loaders';
 
 export const kiotiMigrations: DbMigration[] = [
   kiotiCk20CurrentUsMigration,
   kiotiCk20LoadersMigration,
   kiotiCsCurrentUsMigration,
+  kiotiCsLoadersMigration,
 ];
