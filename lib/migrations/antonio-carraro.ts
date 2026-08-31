dae6f69943be359d5dea70235ba7db0d2d9a9b4f
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-31 09:33:08 +0300

    Register Antonio Carraro TTR attachments

--- lib/migrations/antonio-carraro.ts
+++ lib/migrations/antonio-carraro.ts
@@ -1,6 +1,8 @@
 import type { DbMigration } from '@/lib/db-migration-types';
 import { antonioCarraroNaFeaturedCurrentMigration } from '@/lib/migrations/20260831_471_antonio_carraro_na_featured_current';
+import { antonioCarraroTtr4800AttachmentsMigration } from '@/lib/migrations/20260831_472_antonio_carraro_ttr_4800_attachments';
 
 export const antonioCarraroMigrations: DbMigration[] = [
   antonioCarraroNaFeaturedCurrentMigration,
+  antonioCarraroTtr4800AttachmentsMigration,
 ];
