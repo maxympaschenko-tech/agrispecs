eaef40a8376b5dd6a205fd3826f523f6cb91890f
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-31 09:39:40 +0300

    Register Antonio Carraro Infinity lineup

--- lib/migrations/antonio-carraro.ts
+++ lib/migrations/antonio-carraro.ts
@@ -3,10 +3,12 @@ import { antonioCarraroNaFeaturedCurrentMigration } from '@/lib/migrations/20260
 import { antonioCarraroTtr4800AttachmentsMigration } from '@/lib/migrations/20260831_472_antonio_carraro_ttr_4800_attachments';
 import { antonioCarraroToraNaCurrentMigration } from '@/lib/migrations/20260831_473_antonio_carraro_tora_na_current';
 import { antonioCarraroErgitRNaCurrentMigration } from '@/lib/migrations/20260831_474_antonio_carraro_ergit_r_na_current';
+import { antonioCarraroInfinityNaCurrentMigration } from '@/lib/migrations/20260831_475_antonio_carraro_infinity_na_current';
 
 export const antonioCarraroMigrations: DbMigration[] = [
   antonioCarraroNaFeaturedCurrentMigration,
   antonioCarraroTtr4800AttachmentsMigration,
   antonioCarraroToraNaCurrentMigration,
   antonioCarraroErgitRNaCurrentMigration,
+  antonioCarraroInfinityNaCurrentMigration,
 ];
