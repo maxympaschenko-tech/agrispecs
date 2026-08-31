6d5e484e4543c12ea0b03a7da1ae59eb1f8ae902
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-31 09:36:29 +0300

    Register Antonio Carraro Ergit R lineup

--- lib/migrations/antonio-carraro.ts
+++ lib/migrations/antonio-carraro.ts
@@ -2,9 +2,11 @@ import type { DbMigration } from '@/lib/db-migration-types';
 import { antonioCarraroNaFeaturedCurrentMigration } from '@/lib/migrations/20260831_471_antonio_carraro_na_featured_current';
 import { antonioCarraroTtr4800AttachmentsMigration } from '@/lib/migrations/20260831_472_antonio_carraro_ttr_4800_attachments';
 import { antonioCarraroToraNaCurrentMigration } from '@/lib/migrations/20260831_473_antonio_carraro_tora_na_current';
+import { antonioCarraroErgitRNaCurrentMigration } from '@/lib/migrations/20260831_474_antonio_carraro_ergit_r_na_current';
 
 export const antonioCarraroMigrations: DbMigration[] = [
   antonioCarraroNaFeaturedCurrentMigration,
   antonioCarraroTtr4800AttachmentsMigration,
   antonioCarraroToraNaCurrentMigration,
+  antonioCarraroErgitRNaCurrentMigration,
 ];
