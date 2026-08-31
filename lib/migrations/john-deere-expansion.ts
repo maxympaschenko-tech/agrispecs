931af934eb9d546c5b142ba49c04606a257be681
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-31 15:34:53 +0300

    Register John Deere ST strip-till models

--- lib/migrations/john-deere-expansion.ts
+++ lib/migrations/john-deere-expansion.ts
@@ -39,6 +39,7 @@ import { johnDeereHsdHighSpeedDisksCurrentMigration } from '@/lib/migrations/202
 import { johnDeere2730CombinationRipperCurrentMigration } from '@/lib/migrations/20260831_522_john_deere_2730_combination_ripper_current';
 import { johnDeere2630SeriesTandemDisksCurrentMigration } from '@/lib/migrations/20260831_524_john_deere_2630_series_tandem_disks_current';
 import { johnDeereMtMinimumTillRippersCurrentMigration } from '@/lib/migrations/20260831_527_john_deere_mt_minimum_till_rippers_current';
+import { johnDeereStStripTillCurrentMigration } from '@/lib/migrations/20260831_528_john_deere_st_strip_till_current';
 
 export const johnDeereExpansionMigrations: DbMigration[] = [
   johnDeere7RCurrentSpecsMigration,
@@ -81,4 +82,5 @@ export const johnDeereExpansionMigrations: DbMigration[] = [
   johnDeere2730CombinationRipperCurrentMigration,
   johnDeere2630SeriesTandemDisksCurrentMigration,
   johnDeereMtMinimumTillRippersCurrentMigration,
+  johnDeereStStripTillCurrentMigration,
 ];
