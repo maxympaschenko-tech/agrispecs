eb2f0d14e6a5fa27be52119b29f0afa8c6757119
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-31 11:47:47 +0300

    Register Case IH Patriot sprayer migration

--- lib/migrations/case-ih.ts
+++ lib/migrations/case-ih.ts
@@ -37,6 +37,7 @@ import { caseIHPuma155NewCurrentMigration } from '@/lib/migrations/20260828_261_
 import { caseIHPuma165185NewCurrentMigration } from '@/lib/migrations/20260828_262_case_ih_puma_165_185_new_current';
 import { caseIHFarmall110MStrictCurrentCorrectionMigration } from '@/lib/migrations/20260829_290_case_ih_farmall_110m_strict_current_correction';
 import { caseIHCombinesCurrentMigration } from '@/lib/migrations/20260831_482_case_ih_combines_current';
+import { caseIHPatriotSprayersCurrentMigration } from '@/lib/migrations/20260831_487_case_ih_patriot_sprayers_current';
 
 export const caseIHMigrations: DbMigration[] = [
   caseIHAfsConnectPumaCurrentSpecsMigration, caseIHAfsConnectPumaL117LoaderMigration,
@@ -57,4 +58,5 @@ export const caseIHMigrations: DbMigration[] = [
   caseIHFarmallMediumUtilityALoaderCorrectionsMigration, caseIHPuma155NewCurrentMigration,
   caseIHPuma165185NewCurrentMigration, caseIHFarmall110MStrictCurrentCorrectionMigration,
   caseIHCombinesCurrentMigration,
+  caseIHPatriotSprayersCurrentMigration,
 ];
