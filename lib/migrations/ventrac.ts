6c92f46d77881d3ad3b11135ec57b36674f51d7d
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-31 09:14:00 +0300

    Register Ventrac 45RC migration

--- lib/migrations/ventrac.ts
+++ lib/migrations/ventrac.ts
@@ -1,6 +1,8 @@
 import type { DbMigration } from '@/lib/db-migration-types';
 import { ventrac4520CurrentUsMigration } from '@/lib/migrations/20260831_464_ventrac_4520_current_us';
+import { ventrac45rcCurrentUsMigration } from '@/lib/migrations/20260831_465_ventrac_45rc_current_us';
 
 export const ventracMigrations: DbMigration[] = [
   ventrac4520CurrentUsMigration,
+  ventrac45rcCurrentUsMigration,
 ];
