5ef77f7f0c794b5838fe439b703e0ca8976dedc7
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-09-01 08:31:53 +0300

    Add equipment comparison to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -15,6 +15,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     '',
     '/tractors',
     '/equipment',
+    '/equipment/compare',
     '/brands',
     '/parts',
     '/attachments',
