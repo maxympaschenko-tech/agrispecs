b083068ae8c2ebe13a3e9ee85a268ec16ae0f385
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-09-01 19:50:01 +0300

    Register New Holland Boomer 50 filter migration

--- lib/migrations/new-holland.ts
+++ lib/migrations/new-holland.ts
@@ -41,6 +41,7 @@ import { newHollandMiniExcavatorsCurrentMigration } from '@/lib/migrations/20260
 import { newHollandF50cRoughTerrainForkliftCurrentMigration } from '@/lib/migrations/20260901_560_new_holland_f50c_rough_terrain_forklift_current';
 import { newHollandDSeriesLoaderBackhoesCurrentMigration } from '@/lib/migrations/20260901_561_new_holland_d_series_loader_backhoes_current';
 import { newHollandU80dTractorLoaderCurrentMigration } from '@/lib/migrations/20260901_562_new_holland_u80d_tractor_loader_current';
+import { newHollandBoomer50FiltersPartsMigration } from '@/lib/migrations/20260901_577_new_holland_boomer50_filters_parts';
 
 export const newHollandMigrations: DbMigration[] = [
   newHollandPowerStarCurrentSpecsMigration,
@@ -85,4 +86,5 @@ export const newHollandMigrations: DbMigration[] = [
   newHollandF50cRoughTerrainForkliftCurrentMigration,
   newHollandDSeriesLoaderBackhoesCurrentMigration,
   newHollandU80dTractorLoaderCurrentMigration,
+  newHollandBoomer50FiltersPartsMigration,
 ];
