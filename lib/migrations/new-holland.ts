80a48b899f13d9a51397135e0eb72401cf35df03
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-09-03 15:26:05 +0300

    Use T5 continuation migration registry

--- lib/migrations/new-holland.ts
+++ lib/migrations/new-holland.ts
@@ -1,4 +1,5 @@
 import type { DbMigration } from '@/lib/db-migration-types';
+import { newHollandT5ContinuationMigrations } from '@/lib/migrations/new-holland-t5-continuation';
 import { newHollandPowerStarCurrentSpecsMigration } from '@/lib/migrations/20260828_272_new_holland_powerstar_current_specs';
 import { newHollandWorkmaster5070CurrentSpecsMigration } from '@/lib/migrations/20260828_273_new_holland_workmaster_50_70_current_specs';
 import { newHollandWorkmasterCompactCurrentSpecsMigration } from '@/lib/migrations/20260828_274_new_holland_workmaster_compact_current_specs';
@@ -159,4 +160,5 @@ export const newHollandMigrations: DbMigration[] = [
   newHollandT5130140BrakeReservoirFilterMigration,
   newHollandT5130140PrimaryAirMigration,
   newHollandT5110120PrimaryAirMigration,
+  ...newHollandT5ContinuationMigrations,
 ];
