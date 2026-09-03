6f07beba84aa4d4837709b203d34890b644db53f
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-09-03 15:31:12 +0300

    Register T5.110/T5.120 cab filter migration

--- lib/migrations/new-holland-t5-continuation.ts
+++ lib/migrations/new-holland-t5-continuation.ts
@@ -2,9 +2,11 @@ import type { DbMigration } from '@/lib/db-migration-types';
 import { newHollandT5110120SecondaryAirMigration } from '@/lib/migrations/20260903_615_new_holland_t5_110_120_secondary_air';
 import { newHollandT5110120BrakeReservoirFilterMigration } from '@/lib/migrations/20260903_616_new_holland_t5_110_120_brake_reservoir_filter';
 import { newHollandT5110120LowRoofCabFilterMigration } from '@/lib/migrations/20260903_617_new_holland_t5_110_120_low_roof_cab_filter';
+import { newHollandT5110120CabFilterMigration } from '@/lib/migrations/20260903_618_new_holland_t5_110_120_cab_filter';
 
 export const newHollandT5ContinuationMigrations: DbMigration[] = [
   newHollandT5110120SecondaryAirMigration,
   newHollandT5110120BrakeReservoirFilterMigration,
   newHollandT5110120LowRoofCabFilterMigration,
+  newHollandT5110120CabFilterMigration,
 ];
