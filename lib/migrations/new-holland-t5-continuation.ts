ce96ff74e70838b79860fa1ac4b5e2978f682a3d
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-09-03 15:28:13 +0300

    Register T5.110/T5.120 brake reservoir migration

--- lib/migrations/new-holland-t5-continuation.ts
+++ lib/migrations/new-holland-t5-continuation.ts
@@ -1,6 +1,8 @@
 import type { DbMigration } from '@/lib/db-migration-types';
 import { newHollandT5110120SecondaryAirMigration } from '@/lib/migrations/20260903_615_new_holland_t5_110_120_secondary_air';
+import { newHollandT5110120BrakeReservoirFilterMigration } from '@/lib/migrations/20260903_616_new_holland_t5_110_120_brake_reservoir_filter';
 
 export const newHollandT5ContinuationMigrations: DbMigration[] = [
   newHollandT5110120SecondaryAirMigration,
+  newHollandT5110120BrakeReservoirFilterMigration,
 ];
