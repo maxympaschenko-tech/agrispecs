910679aa6073b2492e1c0b95910de1636707fa8b
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-09-05 13:34:46 +0300

    Add exact T6 Stage V hydraulic filter fitment (#134)
    * Add exact T6 Stage V hydraulic filter fitment
    
    * Register T6 Stage V hydraulic filter migration

--- lib/migrations/new-holland-t6-continuation.ts
+++ lib/migrations/new-holland-t6-continuation.ts
@@ -5,6 +5,7 @@ import { newHollandT6Manual90478716MaintenanceMigration } from '@/lib/migrations
 import { newHollandT6StageVEngineOilFilterMigration } from '@/lib/migrations/20260905_647_new_holland_t6_stagev_engine_oil_filter';
 import { newHollandT6StageVFuelFiltersMigration } from '@/lib/migrations/20260905_648_new_holland_t6_stagev_fuel_filters';
 import { newHollandT6StageVAirFiltersMigration } from '@/lib/migrations/20260905_649_new_holland_t6_stagev_air_filters';
+import { newHollandT6StageVHydraulicFiltersMigration } from '@/lib/migrations/20260905_650_new_holland_t6_stagev_hydraulic_filters';
 
 export const newHollandT6ContinuationMigrations: DbMigration[] = [
   newHollandT6CurrentEnrichmentMigration,
@@ -13,4 +14,5 @@ export const newHollandT6ContinuationMigrations: DbMigration[] = [
   newHollandT6StageVEngineOilFilterMigration,
   newHollandT6StageVFuelFiltersMigration,
   newHollandT6StageVAirFiltersMigration,
+  newHollandT6StageVHydraulicFiltersMigration,
 ];
