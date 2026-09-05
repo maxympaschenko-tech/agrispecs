18cec62eace6c87627387ae177b7126a7dcb33cd
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-09-05 13:32:18 +0300

    Register T6 Stage V air filter migration

--- lib/migrations/new-holland-t6-continuation.ts
+++ lib/migrations/new-holland-t6-continuation.ts
@@ -4,6 +4,7 @@ import { newHollandT6ServiceIntervalsMigration } from '@/lib/migrations/20260905
 import { newHollandT6Manual90478716MaintenanceMigration } from '@/lib/migrations/20260905_646_new_holland_t6_manual_90478716_maintenance';
 import { newHollandT6StageVEngineOilFilterMigration } from '@/lib/migrations/20260905_647_new_holland_t6_stagev_engine_oil_filter';
 import { newHollandT6StageVFuelFiltersMigration } from '@/lib/migrations/20260905_648_new_holland_t6_stagev_fuel_filters';
+import { newHollandT6StageVAirFiltersMigration } from '@/lib/migrations/20260905_649_new_holland_t6_stagev_air_filters';
 
 export const newHollandT6ContinuationMigrations: DbMigration[] = [
   newHollandT6CurrentEnrichmentMigration,
@@ -11,4 +12,5 @@ export const newHollandT6ContinuationMigrations: DbMigration[] = [
   newHollandT6Manual90478716MaintenanceMigration,
   newHollandT6StageVEngineOilFilterMigration,
   newHollandT6StageVFuelFiltersMigration,
+  newHollandT6StageVAirFiltersMigration,
 ];
