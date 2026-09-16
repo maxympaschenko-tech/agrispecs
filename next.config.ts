135a29ef3383c298cc99491f9d15ff29a024a422
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-09-16 18:49:17 +0300

    Avoid chained canonical URL redirects

--- next.config.ts
+++ next.config.ts
@@ -12,21 +12,7 @@ const nextConfig: NextConfig = {
   poweredByHeader: false,
   reactStrictMode: true,
   trailingSlash: false,
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.farmmachinespecs.com',
-          },
-        ],
-        destination: 'https://farmmachinespecs.com/:path*',
-        permanent: true,
-      },
-    ];
-  },
+  skipTrailingSlashRedirect: true,
   async headers() {
     return [
       {
